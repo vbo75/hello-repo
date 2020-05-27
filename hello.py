3a40631dbd41faa36909949c94b9a27da40fc049
vbo75 <boulet.vincent@gmail.com>
2020-05-27 14:48:09 +0200

    Ask for name

--- hello.py
+++ hello.py
@@ -2,5 +2,8 @@ import numpy as np
 
 print("Hello World !")
 
+name = input("What's your name ? ")
+print("Please to meet you", name) 
+
 days_left = np.datetime64('2021-07-23') - np.datetime64('today')
 print("We are", days_left, "away from Tokyo olympic games...")
